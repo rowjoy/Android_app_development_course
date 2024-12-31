4336d0a0d85c3e7a90511437b17c05c43b818d90
NecJoy <joy@primotechltd.com>
2024-12-31 15:53:55 +0600

    Project modify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.tracker.androidcourse"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.tracker.androidcourse"
---
+++ app_note.kt
@@ -0,0 +1,33 @@
+// https://youtu.be/ERBEWmfz6h0?si=0TexLj0_8gM3Xghg
+
+/*
+Mian link : https://www.youtube.com/watch?v=ERBEWmfz6h0&list=PLSrm9z4zp4mEWwyiuYgVMWcDFdsebhM-r
+
+*  Day 1 -> Introduction - Jetpack Compose | https://youtu.be/ERBEWmfz6h0?si=q1bzH6adUAoEvBf7
+
+*  Day 2 -> Recomposition - Jetpack Compose | https://youtu.be/O6zNcV3PaJE?si=MgCL3NPByDytmznf
+
+*  Day 3 -> States - Jetpack Compose | https://youtu.be/gQWrP8YGzWE?si=v3g8mfmVQNb6euix
+
+* New  Day 4 -> Write Your First Composable Function! - Jetpack Compose | https://youtu.be/5yde-kGgdk0?si=7DfS3eP47s9GzEXI
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* */
