8767ef472c038c3dda00ccd2ed86884fc00cfe1c
rowjoy <rowjoy202@gmail.com>
2025-01-29 20:45:54 +0600

    IndicatorWidget

--- app/src/main/java/com/tracker/androidcourse/Indicator.kt
+++ app/src/main/java/com/tracker/androidcourse/Indicator.kt
@@ -1,8 +1,12 @@
 package com.tracker.androidcourse
 
+import androidx.compose.animation.Animatable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -14,38 +18,63 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.animation.core.*
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
 
 
 @Composable
 fun IndicatorWidget (
     canvasSize: Dp = 300.dp,
-  //  indicatorBgColor :  Color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f,),
+    indicatorValue : Int = 0,
+    maxIndicatorValue : Int = 1000,
     indicatorBgColor : Color = Color.Gray.copy(alpha = 0.2f),
-    startAngle: Float = 150f,
-    sweepAngle: Float = 250f,
-    useCenter: Boolean = false,
-    bgStockwidth: Float = 100f
-){
-   Column (
+    indicatorValueColor: Color = Color.Blue,
+    foregroundStartAngle: Float = 150f,
+    bgStockwidth: Float = 100f,
 
+){
+
+
+
+   val animatedIndicatorValue  = remember { Animatable(initialValue = 0f)}
+
+   LaunchedEffect(key1 = indicatorValue) {
+       animatedIndicatorValue.animateTo(indicatorValue.toFloat())
+   }
+
+   val percentage = (animatedIndicatorValue.value / maxIndicatorValue) * 100;
+
+   val sweepAngle by animateFloatAsState(
+       targetValue = (2.4 * percentage).toFloat(),
+       animationSpec = tween(1000)
+   );
+
+
+   Column (
        modifier = Modifier
            .size(canvasSize)
            .drawBehind {
                val componentSize = size / 1.2f;
                backgroundIndicator( // this is backgroundIndicator component
                    indicatorColor = indicatorBgColor,
-                   startAngle = startAngle,
-                   sweepAngle = sweepAngle,
-                   useCenter = useCenter,
+                   startAngle = 150f,
+                   sweepAngle = 250f,
+                   useCenter = false,
                    componentSize = componentSize,
                    width = bgStockwidth
                );
                foregroundIndicator(
-                   foregroundIndicatorSize = componentSize
+                   foregroundIndicatorSize = componentSize,
+                   startAngle = foregroundStartAngle,
+                   sweepAngle = sweepAngle,
+                   indicatorValueColor = indicatorValueColor,
+                   width = bgStockwidth
                );
-           }
+           },
    ) {
 
    }
@@ -59,7 +88,7 @@ fun IndicatorPreview (){
 
 
 fun DrawScope.backgroundIndicator (
-    indicatorColor :  Color,
+    indicatorColor : Color,
     startAngle: Float,
     sweepAngle: Float,
     useCenter: Boolean,
@@ -88,15 +117,19 @@ fun DrawScope.backgroundIndicator (
 
 fun DrawScope.foregroundIndicator (
     foregroundIndicatorSize : Size,
+    startAngle: Float,
+    sweepAngle: Float,
+    indicatorValueColor: Color = Color.Red,
+    width: Float
 ){
    drawArc(
        size = foregroundIndicatorSize,
-       color = Color.Red,
-       startAngle = 150f,
-       sweepAngle = 200f,
+       color = indicatorValueColor,
+       startAngle = startAngle,
+       sweepAngle = sweepAngle,
        useCenter = false,
        style = Stroke(
-         width = 100f,
+         width = width,
            cap = StrokeCap.Round
        ),
        topLeft = Offset(
@@ -105,6 +138,10 @@ fun DrawScope.foregroundIndicator (
        )
    );
 }
+
+
+// https://www.youtube.com/watch?v=tWSwbTJjJUg&list=PLSrm9z4zp4mEWwyiuYgVMWcDFdsebhM-r&index=22
+// Custom UI Component with Jetpack Compose & Canvas | Part #3 - Foreground Indicator
 
 
 
